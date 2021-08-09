7dfdb8df71d59d31572094bf8be0f9bda5fc5029
rmcs87 <rmcs87@gmail.com>
2021-08-09 08:39:26 -0300

    Fim de Aula.

---
+++ 02_hello_poo/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Cat
+{
+    public Cat(int age, string nome)
+    {
+        Age = age;
+        Nome = nome;
+    }
+    public int Age { get ; set; }
+    public string Nome { get; set; }
+    public void Mia()
+    {
+        Console.WriteLine($"{Nome} says: Meow.");
+    }
+}
+
---
+++ 02_hello_poo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cat A = new Cat(8, "Mia");
+        Cat B = new Cat(1, "Kat");
+
+        A.Mia();
+        B.Mia();
+    }
+}
+
