9dae7faa9c186a33296904f4a17e935d74e90d2e
rmcs87 <rmcs87@gmail.com>
2021-08-13 08:33:44 -0300

    Timer done

---
+++ 03_timer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _03_timer
+{
+    class Program
+    {
+        static void Main(string[] args){
+            int[] nums = new int[1000000];
+            
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            BuildArray(nums);
+            sw.Stop();
+            Console.WriteLine($"Tempo para construir = {sw.Elapsed}");
+        }
+
+        static void BuildArray(int[] arr){
+            Random r = new Random();
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = r.Next();
+            }
+        }
+        static void PrintArray(int[] arr){
+            foreach (var item in arr)
+            {
+                Console.WriteLine($"Valor = {item}");
+            }
+        }
+
+    }
+}
