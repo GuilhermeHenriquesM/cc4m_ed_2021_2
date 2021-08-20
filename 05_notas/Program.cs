36da28affd62ca71896946804a89915a235713fb
rmcs87 <rmcs87@gmail.com>
2021-08-20 08:25:14 -0300

    Final de aula

--- 05_notas/Program.cs
+++ 05_notas/Program.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 
-
 class Program
 {
     static void Main(string[] args)
     {
+        Curso curso = new Curso("ED");
+
+        curso.Notas.Add((decimal)5.0);
+        curso.Notas.Add((decimal)6.1);
+        curso.Notas.Add((decimal)6.1);
+        curso.Notas.Add((decimal)6.1);
+        curso.Notas.Add((decimal)6.1);
+        curso.Notas.Add((decimal)6.1);
+
+        Console.WriteLine($"Media de {curso.Nome}={curso.MediaNotas()}");
+        Console.WriteLine($"Maior de {curso.Nome}={curso.MaiorNota()}");
+        Console.WriteLine($"Menor de {curso.Nome}={curso.MenorNota()}");
+
         Console.WriteLine("Hello World!");
     }
 }
