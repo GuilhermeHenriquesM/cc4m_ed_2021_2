de0b5025a16e77f66463bbdf4f0c609b6dba7f6a
rmcs87 <rmcs87@gmail.com>
2021-08-23 08:20:05 -0300

    Buble primeira parte

---
+++ 06_ordenacao_simples/Ordenacao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Ordenacao
+{
+    public void Bubblesort(int[] vet)
+    {
+        for (int i = 0; i < vet.Length - 1; i++)
+        {
+            if (vet[i] > vet[i+1])
+            {
+                int aux = vet[i];
+                vet[i] = vet[i + 1];
+                vet[i + 1] = aux;
+            }
+        }
+    }
+    public void Insertionsort(int[] vet)
+    {
+
+    }
+    public void Selectionsort(int[] vet)
+    {
+
+    }
+}
---
+++ 06_ordenacao_simples/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Inicializar vetor
+        int[] vetor1 = new int[10];
+        Random rand = new Random();
+        for (int i = 0; i < vetor1.Length; i++)
+        {
+            vetor1[i] = rand.Next(0, 100);
+        }
+
+        //Ordenar
+        Ordenacao ordenacao = new Ordenacao();
+        ordenacao.Bubblesort(vetor1);
+
+        //Exibir
+        foreach (var item in vetor1)
+        {
+            Console.Write($"{item} - ");
+        }
+    }
+}
